05c8ff5317a0b20b18837ce212e982254f656de0
Abu Jafor Saleh <33859928+abujaforsaleh@users.noreply.github.com>
2025-07-21 08:35:16 +0600

    Update auth_service.dart
    dio functionality added to authservice

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,21 +1,51 @@
 import 'dart:developer';
+import 'package:dio/dio.dart';
+import 'dio_client.dart';
 
 class AuthService {
+  final Dio _dio = DioClient.dio;
+
   Future<bool> login(String email, String password) async {
-    log('API CALL: login() - $email / $password');
-    await Future.delayed(const Duration(seconds: 2)); // Simulate network
-    return email == "demo@mail.com" && password == "123456";
+    try {
+      final res = await _dio.post('/login', data: {
+        'email': email,
+        'password': password,
+      });
+
+      log('Login success: ${res.data}');
+      return true;
+    } on DioException catch (e) {
+      log('Login error: ${e.response?.data ?? e.message}');
+      return false;
+    }
   }
 
   Future<bool> register(String email, String password) async {
-    log('API CALL: register() - $email / $password');
-    await Future.delayed(const Duration(seconds: 2));
-    return true;
+    try {
+      final res = await _dio.post('/register', data: {
+        'email': email,
+        'password': password,
+      });
+
+      log('Register success: ${res.data}');
+      return true;
+    } on DioException catch (e) {
+      log('Register error: ${e.response?.data ?? e.message}');
+      return false;
+    }
   }
 
   Future<bool> verifyOtp(String code) async {
-    log('API CALL: verifyOtp() - code: $code');
-    await Future.delayed(const Duration(seconds: 2));
-    return code == "123456"; // pretend 123456 is the correct OTP
+    try {
+      final res = await _dio.post('/verify-otp', data: {
+        'otp': code,
+      });
+
+      log('OTP verified: ${res.data}');
+      return true;
+    } on DioException catch (e) {
+      log('OTP verify error: ${e.response?.data ?? e.message}');
+      return false;
+    }
   }
 }
