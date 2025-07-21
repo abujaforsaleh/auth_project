28c6c467908d2c354ace53967a1747c6d54a34f3
Abu Jafor Saleh <33859928+abujaforsaleh@users.noreply.github.com>
2025-07-21 08:38:48 +0600

    Update dio_client.dart
    base url string modified

--- lib/core/services/dio_client.dart
+++ lib/core/services/dio_client.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class DioClient {
   static final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: 'https://your-api-url.com/api', // 👈 Replace this
+      baseUrl: 'https://base_url.com/api', // 👈 Replace this
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       headers: {
