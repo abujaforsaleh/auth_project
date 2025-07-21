12211f6edc88aeac898ccea502d7bbd9ce63059f
Abu Jafor Saleh <33859928+abujaforsaleh@users.noreply.github.com>
2025-07-21 08:33:50 +0600

    Create dio_client.dart
    dio client added

---
+++ lib/core/services/dio_client.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  static final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://your-api-url.com/api', // 👈 Replace this
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
+  static Dio get dio => _dio;
+}
